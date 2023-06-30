bffffc8b5b78567bb8a0b681c0d0e6b4c5be269c
dalokraff <87997987+dalokraff@users.noreply.github.com>
2023-06-29 22:08:48 -0500

    add vamp and dwarf maps to old ui

---
+++ scripts/mods/old-map-select/old_ui/area_settings/karak_azgaraz.lua
@@ -0,0 +1,23 @@
+local area_settings = {
+    dlc_dwarf_beacons = {
+        pos_x = -210,
+        pos_y = -300, 
+        -- scale = 0.3, 
+        level_image = "achievement_wizards_trail_light_bonfires"
+    },
+    dlc_dwarf_exterior = {
+        pos_x = 225,
+        pos_y = -55,
+        -- scale = 0.3,
+        level_image = "achievement_exterior_meta"
+    },
+    dlc_dwarf_interior = {
+        pos_x = -365,
+        pos_y = 50,
+        -- scale = 0.3,
+        level_image = "achievement_dwarf_valaya_emote"
+    },
+}
+
+
+return area_settings
---
+++ scripts/mods/old-map-select/old_ui/area_settings/penny.lua
@@ -0,0 +1,23 @@
+local area_settings = {
+    dlc_portals = {
+        pos_x = 312,
+        pos_y = 130, 
+        -- scale = 0.3, 
+        level_image = "achievement_trophy_penny_complete_portals"
+    },
+    dlc_bastion = {
+        pos_x = 225,
+        pos_y = -212.5,
+        -- scale = 0.3,
+        level_image = "achievement_trophy_penny_bastion_torch"
+    },
+    dlc_castle = {
+        pos_x = -25,
+        pos_y = -325,
+        -- scale = 0.3,
+        level_image = "achievement_trophy_penny_complete_castle"
+    },
+}
+
+
+return area_settings
